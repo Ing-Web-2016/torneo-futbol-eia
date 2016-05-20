aa7768ba91181cb8ba1e4aeaeda7f423cc78c456
Julian Vanegas <julianvanegas357@gmail.com>
2016-05-20 11:34:36 -0500

    Fixed ignored files

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=127.0.0.1;dbname=torneo-futbol-eia',
     'username' => 'root',
     'password' => '',
     'charset' => 'utf8',
