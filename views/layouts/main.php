6d56831504ef4cb7332daac18e4a5dd85ded05fe
SandraTobon <hemionetoboso@hotmail.com>
2016-05-26 16:37:57 -0500

    Update main.php

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -1,3 +1,4 @@
+
 <?php
 
 /* @var $this \yii\web\View */
@@ -6,6 +7,7 @@
 use yii\helpers\Html;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
+use yii\bootstrap\Carousel;
 use yii\widgets\Breadcrumbs;
 use app\assets\AppAsset;
 
@@ -23,39 +25,56 @@ AppAsset::register($this);
 </head>
 <body>
 <?php $this->beginBody() ?>
-
-<div class="wrap">
-    <?php
+<div class="row">
+<?php
     NavBar::begin([
-        'brandLabel' => 'My Company',
-        'brandUrl' => Yii::$app->homeUrl,
         'options' => [
-            'class' => 'navbar-inverse navbar-fixed-top',
+            'class' => 'navbar navbar-fixed-top',
+            'id'=>'navegador',
+            
         ],
     ]);
     echo Nav::widget([
-        'options' => ['class' => 'navbar-nav navbar-right'],
-        'items' => [
-            ['label' => 'Home', 'url' => ['/site/index']],
-            ['label' => 'About', 'url' => ['/site/about']],
-            ['label' => 'Contact', 'url' => ['/site/contact']],
-            Yii::$app->user->isGuest ? (
-                ['label' => 'Login', 'url' => ['/site/login']]
-            ) : (
-                '<li>'
-                . Html::beginForm(['/site/logout'], 'post', ['class' => 'navbar-form'])
-                . Html::submitButton(
-                    'Logout (' . Yii::$app->user->identity->username . ')',
-                    ['class' => 'btn btn-link']
-                )
-                . Html::endForm()
-                . '</li>'
-            )
+        'options' => ['class'=>'nav nav-justified'],
+        'items' => [ 
+            ['label' => 'INICIO', 'url' => ['/site/inicio'], 'linkOptions'=>['id'=>'letra']], 
+            ['label' => 'LOGIN', 'url' => ['/site/login'], 'linkOptions'=>['id'=>'letra']],
+            ['label' => 'JUGADORES', 'url' => ['/site/jugadores'], 'linkOptions'=>['id'=>'letra']],
+            ['label' => 'EQUIPOS', 'url' => ['/site/equipos'], 'linkOptions'=>['id'=>'letra']],
+            ['label' => 'PARTIDOS', 'url' => ['/site/partidos'], 'linkOptions'=>['id'=>'letra']]
         ],
     ]);
     NavBar::end();
     ?>
+    </div>
+<div class="wrap">
+<div class="row" id="cabezote">
+    <div class="col-md-2"></div>
+    <div class="col-md-8">
+        
+    
+    <?php
+    echo Carousel::widget([
+    'items' => [
+        '<img id="imagen" src="http://localhost/torneo-futbol-eia/views/layouts/img/eia1.jpeg"/>',
+        '<img id="imagen" src="http://localhost/torneo-futbol-eia/views/layouts/img/eia2.jpeg"/>',
+        '<img id="imagen" src="http://localhost/torneo-futbol-eia/views/layouts/img/eia3.jpeg"/>',
+        '<img id="imagen" src="http://localhost/torneo-futbol-eia/views/layouts/img/eia4.jpeg"/>',
+        '<img id="imagen" src="http://localhost/torneo-futbol-eia/views/layouts/img/eia5.jpeg"/>',
+        '<img id="imagen" src="http://localhost/torneo-futbol-eia/views/layouts/img/eia6.jpeg"/>',
+    ],
+    'options'=>[
+     'icon'=>('GLYPHICON_CIRCLE_ARROW_LEFT'),
+    'icon'=>('GLYPHICON_CIRCLE_ARROW_RIGHT'),
+    ],
+    ]);
+    ?>
+    
+    </div>
+    <div class="col-md-2"></div>
+    </div>
 
+    
     <div class="container">
         <?= Breadcrumbs::widget([
             'links' => isset($this->params['breadcrumbs']) ? $this->params['breadcrumbs'] : [],
@@ -65,11 +84,7 @@ AppAsset::register($this);
 </div>
 
 <footer class="footer">
-    <div class="container">
-        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
-
-        <p class="pull-right"><?= Yii::powered() ?></p>
-    </div>
+    <img src="http://localhost/torneo-futbol-eia/views/layouts/img/pie.jpg" class="img-responsive center-block" id="pie">
 </footer>
 
 <?php $this->endBody() ?>
